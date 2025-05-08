43bfb9e3dd65a9b40d9e92cbcf9b81f6d2f4dcc0
Noor <147630452+n00rislam@users.noreply.github.com>
2025-05-08 14:15:24 +0500

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,31 +1,13 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
+  version = "~> 19.15.3"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.29"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-  }
-
-  eks_managed_node_groups = {
-    main = {
-      name = "node-group-1"
-
-      instance_types = ["t3.medium"]
-
-      min_size     = 2
-      max_size     = 4
-      desired_size = 2
-
-      vpc_security_group_ids = [aws_security_group.node_group_one.id]
-    }
-  }
+  enable_irsa = true
 
   tags = local.tags
 }
